389d9360318c7251d20723bcc55b0613cab7f978
willkretschmer <willkretschmer@yahoo.com>
2025-07-14 12:11:20 -0500

    Add initial Flask app

--- app.py
+++ app.py
@@ -82,3 +82,15 @@ def gather():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port, debug=True)
+----------------------------------------------------------------
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Flask!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
